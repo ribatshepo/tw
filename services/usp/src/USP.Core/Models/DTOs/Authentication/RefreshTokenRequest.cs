3ad471854730a6003cc9146ec0c163ed158aaa3c
tshepo <tshepo.dev.riba@gmail.com>
2025-12-26 16:47:31 +0200

    refactor

--- services/usp/src/USP.Core/Models/DTOs/Authentication/RefreshTokenRequest.cs
+++
@@ -1,9 +0,0 @@
-namespace USP.Core.Models.DTOs.Authentication;
-
-/// <summary>
-/// Request model for token refresh
-/// </summary>
-public class RefreshTokenRequest
-{
-    public string RefreshToken { get; set; } = string.Empty;
-}
